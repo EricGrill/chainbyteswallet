a10c02c3ae9a236540799edbcbd8d09f1751e14c
hitsnorth <eric@chainbytes.com>
2018-07-30 17:48:30 -0400

    Add Dynamic Fees Add OpReturn To Transaction

--- src/wallet.js
+++ src/wallet.js
@@ -3,18 +3,47 @@ const logger = require('winston');
 logger.level = "debug";
 const apiCall = require('../src/apiCall.js');
 const pushtx = require('blockchain.info/pushtx');
-let fee = 50000;  // Need to create a more dynamic fee
+//let fee = 50000;  // Need to create a more dynamic fee
+let calcSize = (Transaction) => {
+    const txSize = (Transaction.tx.ins.length * 180 + Transaction.tx.outs.length * 34 + 10 + -Transaction.tx.ins.length);
+    return txSize; // Transaction size in Bytes
+
+}
+let calcFee = (Transaction, feeType) => {
+    return new Promise((fulfill, reject) => {
+        const size = calcSize(Transaction);
+        let query = 'https://bitaps.com/api/fee';
+        let feeBase = 0;
+        apiCall.getData(query).then((feeData) => {
+            switch (feeType) {
+                case 'medium':
+                    feeBase = feeData.medium;
+                case 'high':
+                    feeBase = feeData.high;
+                default:
+                    feeBase = feeData.low;
+            }
+            if (size * feeBase < 20000) {  // Minimum relay fee
+                fulfill(20000);
+            }
+            fulfill(size * feeBase);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}
 module.exports = {
     getNewAddress: () => {
         const keyPair = bitcoin.ECPair.makeRandom();
         return {address: keyPair.getAddress(), wif: keyPair.toWIF()};
     },
-    calculateSize: (Transaction) => {
-        const txSize = (Transaction.tx.ins.length * 180 + Transaction.tx.outs.length * 34 + 10 + -Transaction.tx.ins.length);
-        return txSize; // Transaction size in Bytes
-    },
-    pushPayment: (payees, payor) => {
+    calculateSize: calcSize,
+    // feeType low,medium,high
+    calculateFee: calcFee,
+
+    pushPayment: (payees, payor, opcode) => {
         return new Promise((fulfill, reject) => {
+            const data = new Buffer(opcode);
             apiCall.getData("https://www.bitstamp.net/api/ticker/").then((btcprice) => {
                 const priceBTC = btcprice.ask;
                 let query = "https://blockchain.info/address/" + payor.address + "?format=json";
@@ -43,9 +72,13 @@ module.exports = {
                         }
                         //   console.log(data.txs[q]);
                     }
-                    if (fee > amount) {  // Sending dust
-                        logger.debug("Fee is more than amount sending");
-                        fee = amount;
+                    if (opcode) {
+                        ret = bitcoin.script.compile(
+                            [
+                                bitcoin.opcodes.OP_RETURN,
+                                data
+                            ])
+                        tx.addOutput(ret, 0);
                     }
                     if (Total < amount && process.env.NODE_ENV != undefined) {
                         reject("Not enough coin to pay total " + Total + "  " + amount);
@@ -53,30 +86,32 @@ module.exports = {
                     }
                     logger.log('debug', "Total: " + Total);
                     logger.log('debug', "Amount: " + amount);
-                    logger.log('debug', "Fee: " + fee);
-                    logger.log('info', 'Sending back %d', Number(Total - amount - fee));
                     payees.forEach((payee) => {
                         logger.log('debug', payee);
                         tx.addOutput(payee.wallet, payee.btcamount);
                     });
-                    tx.addOutput(payor.address, Number(Total - (amount + fee)));
-                    // console.log(tx);
-                    tx.sign(0, key);
-                    logger.log("info", tx);
-                    const lengthTransaction = tx.build().toHex().length / 2;
-                    console.log("Transaction size: ", lengthTransaction);
-                    if (process.env.NODE_ENV == undefined) {  // if there is no NODE_ENV its in production and actually send
-                        console.log("Sending");
-                        pushtx.pushtx(tx.build().toHex(), null).then((result) => {
-                            fulfill(amount)
-                        }).catch(function (err) {
-                            reject(err);
-                        });
-                    }
-                    else {
-                        logger.log('info', 'Dev environment not sending');
-                        fulfill(amount);
-                    }
+                    calcFee(tx).then((fee) => {
+                        tx.addOutput(payor.address, Number(Total - (amount + fee)));
+                        logger.log('debug', "Fee: " + fee);
+                        logger.log('info', 'Sending back %d', Number(Total - amount - fee));
+                        tx.sign(0, key);
+                        logger.log("info", tx);
+                        const lengthTransaction = tx.build().toHex().length / 2;
+                        console.log("Transaction size: ", lengthTransaction);
+                        if (process.env.NODE_ENV == undefined) {  // if there is no NODE_ENV its in production and actually send
+                            console.log("Sending");
+                            pushtx.pushtx(tx.build().toHex(), null).then((result) => {
+                                fulfill(amount)
+                            }).catch(function (err) {
+                                reject(err);
+                            });
+                        }
+                        else {
+                            logger.log('info', 'Dev environment not sending');
+                            fulfill(amount);
+                        }
+                    })
+
                 });
             })
         });
