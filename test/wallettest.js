6d01dba01f279013ed6b3f11a03989161c982913
hitsnorth <eric@chainbytes.com>
2018-07-30 19:00:52 -0400

    calculateSizeTest

--- test/wallettest.js
+++ test/wallettest.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 process.env.NODE_ENV='dev';
+const bitcoin = require('bitcoinjs-lib')
 const payee = [{"name": "Person1", "wallet": "1983LDmpGo1iBjz9AAqNEStcaGiya8Wx8N", "amount": 66.67},
     {"name": "Person2", "wallet": "17J1VUivqLNzw89k5aEfZz3rBk9Pg7hWyY", "amount": 66.67}]
 const should = require('chai').should(),
@@ -14,9 +15,18 @@ describe('Wallet', function () {
     });
     describe("#pushPayment", () => {
         it("Pushing payment", () => {
-            wallet.pushPayment(payee, {address: newAddress.address, wif: newAddress.wif}).then((result) => {
+            wallet.pushPayment(payee, {address: newAddress.address, wif: newAddress.wif},'TestCode').then((result) => {
                 result.should.be.a('object');
             });
         });
     })
+    describe("#calculateSize", () => {
+        it("checkingSize", () => {
+            tx = new bitcoin.TransactionBuilder();
+            wallet.calculateFee(tx).then((result) => {
+                result.should.be.a('number');
+            });
+        });
+    })
 });
+
